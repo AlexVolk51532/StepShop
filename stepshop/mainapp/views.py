07ffe143f06091b7d4f66940fa5870768ffbf470
Александр Волков <alex_sashok@mail.ru>
2023-03-02 21:29:22 +0600

    lesson_03: href

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+links_menu = [
+        {'href' : 'index', 'name': 'Главная'},
+        {'href' : 'products:index', 'name': 'Продукт'},
+        {'href' : 'about', 'name': 'О нас'},
+        {'href' : 'contacts', 'name': 'Контакты'},
+    ]
+
 def products(request):
-    return render(request, 'products.html')
+    title = "продукты"
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'products.html', context)
--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
 
+links_menu = [
+        {'href' : 'index', 'name': 'Главная', 'route': ''},
+        {'href' : 'products:index', 'name': 'Продукт', 'route': 'products/'},
+        {'href' : 'about', 'name': 'О нас', 'route': 'about/'},
+        {'href' : 'contacts', 'name': 'Контакты', 'route': 'contacts/'},
+    ]
+
 def index(request):
-    return render(request, 'index.html')
+    title = "Главная"
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'index.html', context)
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = "контакты"
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'contacts.html', context)
+
+def about(request):
+    title = "о нас"
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'about.html', context)
+
+def products(request):
+    title = "продукты"
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'products.html', context)
